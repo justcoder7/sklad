780c68d2dc64f2ba60b53ce416d77c060e733da3
justcoder7 <jahongirnaqshbandov@gmail.com>
2023-10-24 17:48:13 +0500

    Documentation updated

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,18 +2,18 @@ import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/c
 import { ProductService } from './product.service';
 import { createProductDto } from './dto/createproduct.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Product')
 @Controller('product')
 export class ProductController {
     constructor (private productService : ProductService) {}
-
+    
+    @ApiOperation({summary : 'Creating the product'})
+    @ApiResponse({status : 200})
     @Post()
     @UseInterceptors(FileInterceptor('image'))
     createPost(@Body() dto : createProductDto, @UploadedFile() image ){
-        console.log("dsadaghgfhghfhgs")
-        console.log("dsadaghgfhghfhgs")
-        console.log("dsadaghgfhghfhgs")
-        console.log("dsadaghgfhghfhgs")
         return this.productService.create(dto,image)
     }
 }
